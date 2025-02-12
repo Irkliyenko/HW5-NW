dd4d38e4e50677a3f57659e1469131b835371668
Iryna Irkliyenko <iryna.irklyenko@ucsf.edu>
2025-02-12 14:27:45 -0800

    main

--- main.py
+++ main.py
@@ -8,19 +8,45 @@ def main():
     (2) Print all alignment scores between each species BRD2 and human BRD2
     """
     hs_seq, hs_header = read_fasta("./data/Homo_sapiens_BRD2.fa")
-    gg_seq, gg_header = read_fasta("./data/Gallus_gallus_BRD2.fa")
-    mm_seq, mm_header = read_fasta("./data/Mus_musculus_BRD2.fa")
-    br_seq, br_header = read_fasta("./data/Balaeniceps_rex_BRD2.fa")
-    tt_seq, tt_header = read_fasta("./data/tursiops_truncatus_BRD2.fa")
+
+    # Create a dictionary where the key is the species name and the value is its sequence
+    species_data = {
+        "Gallus gallus": read_fasta("./data/Gallus_gallus_BRD2.fa")[0],
+        "Mus musculus": read_fasta("./data/Mus_musculus_BRD2.fa")[0],
+        "Balaeniceps rex": read_fasta("./data/Balaeniceps_rex_BRD2.fa")[0],
+        "Tursiops truncatus": read_fasta("./data/tursiops_truncatus_BRD2.fa")[0]
+    }
 
     # TODO Align all species to humans and print species in order of most similar to human BRD
     # using gap opening penalty of -10 and a gap extension penalty of -1 and BLOSUM62 matrix
-    pass
+    sub_mat = './substitution_matrices/BLOSUM62.mat'
 
-    # TODO print all of the alignment score between each species BRD2 and human BRD2
-    # using gap opening penalty of -10 and a gap extension penalty of -1 and BLOSUM62 matrix
-    pass
-    
+    # Initialize the Needleman-Wunsch class
+    al = NeedlemanWunsch(sub_mat, gap_open = -10, gap_extend = -1)
+
+    # Initialize a dictionary to store species and their alignment scores
+    aln_score = {}
+
+    # Align each species to human sequence
+    for species, seq in species_data.items():
+        score, aligned_hs, aligned_species = al.align(hs_seq, seq)
+        aln_score[species] = score
+
+    # Sort the dictionary by alignment score in descending order (most similar species first)
+    sorted_dict = sorted(aln_score.items(), key=lambda x:x[1])
+
+    # Print the species name and its alignment score
+    for key, v in sorted_dict:
+        print(f"Species: {key}\nAlignment score: {v}\n")
+
+    seq3, _ = read_fasta("./data/test_seq3.fa")
+    seq4, _ = read_fasta("./data/test_seq4.fa") 
+
+    sc, s3, s4 = al.align(seq3, seq4)
+    print(sc)
+    print(s3)
+    print(s4)
+
 
 if __name__ == "__main__":
     main()
